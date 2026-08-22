6d8ae4af0d33e37af2664c5bebf141ac1f382f35
“DerPommeDeTerre” <leobardo.jmaya@gmail.com>
2026-08-21 21:01:45 -0600

    Ejercicio Interfaz Java

---
+++ JavaInterface/JavaInterfaz/src/javainterfaz/Cliente.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javainterfaz;
+
+/**
+ *
+ * @author administrador
+ */
+
+
+//*****************Implementar la interfaz IComprar******************
+public class Cliente {
+    
+    private String idCliente;
+    private String nombre;
+    private String apellido;
+    private int edad;
+
+    public Cliente() {
+    }
+
+    public Cliente(String idCliente, String nombre, String apellido, int edad) {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }        
+    
+    //************Implementar la Interfaz IComprar*********************
+    //************Con las siguientes impresiones en pantalla*********************
+   
+    //System.out.println(nombre + " compró " + producto);
+    //System.out.println("Pagó: " + precio);
+ 
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    
+}
---
+++ JavaInterface/JavaInterfaz/src/javainterfaz/JavaInterfaz.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javainterfaz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author administrador
+ */
+public class JavaInterfaz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Cliente clienteUno = new Cliente("01", "Pablo", "Martínez", 25);
+        Cliente clienteDos = new Cliente("02", "Ximena", "Mariscal", 35);
+        
+        clienteUno.comprar("Batería de acero inoxidable, 10 piezas", 3000);
+        clienteDos.comprar("Horno de convección", 4000);
+        
+        List<Cliente> listaClientes = new ArrayList<>();
+        
+        listaClientes.add(clienteUno);
+        listaClientes.add(clienteDos);
+        
+        
+        //Arreglar el ENHANCED FOR para desplegar los datos de los clientes****************
+        for(elemento:listaClientes){
+            
+            System.out.println("***************************************");
+            System.out.println("Cliente ID: " + elemento.getIdCliente());
+            System.out.println("Nombre " + elemento.getNombre());
+            System.out.println("Apellido " + elemento.getApellido());
+            System.out.println("Edad " + elemento.getEdad());            
+        }
+              
+        
+    }
+    
+}
